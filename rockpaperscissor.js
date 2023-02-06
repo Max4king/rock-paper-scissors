bc8b4b0e7c0b1c37e9fe2cb7b30403bdef0221ac
Max4king <ryan3311@hotmail.com>
2023-02-06 16:14:54 +0100

    first update still not working

---
+++ rockpaperscissor.js
@@ -0,0 +1,10 @@
+//let playerSelection = parseInt(prompt("Choose from 0 to 2: "));
+const choice = ["rock","paper","scissors"];
+
+function getComputerChoice() {
+    return (Math.floor(Math.random() * 10)%3);
+}
+
+function winner_loser(player, computer) {
+
+}
